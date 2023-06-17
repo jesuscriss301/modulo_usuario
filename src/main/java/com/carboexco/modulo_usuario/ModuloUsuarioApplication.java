de7d8dd6b03a50763a62427b09563dd30f5dd7cb
Carboexco2 <124207086+Carboexco2@users.noreply.github.com>
2023-06-17 08:18:58 -0500

    primer commit

---
+++ src/main/java/com/carboexco/modulo_usuario/ModuloUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.carboexco.modulo_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloUsuarioApplication.class, args);
+	}
+
+}
